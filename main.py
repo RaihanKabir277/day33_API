1a04b5f8095d35cd152cded021523e071cd5983e
RaihanKabir277 <raihank0198@gmail.com>
2025-09-03 11:33:33 +0600

    pass the params of lat lng and the name of the params should equal to the api need

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 import requests 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 51.507351
+MY_LON = -0.127758
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # print(response)
 # print(type(response))
 
@@ -15,7 +19,7 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 #     raise Exception("you are not authorised to access this data")
 
 #------------ there is a error handing in request documentation -------
-response.raise_for_status()
+# response.raise_for_status()
 
 # ----- json data fetch ----
 # data = response.json()
@@ -28,11 +32,23 @@ response.raise_for_status()
 # print(data)
 
 # -------paricularly fetch longtitude and latitude -------
+# data = response.json()
+
+# longtitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longtitude, latitude)
+# print(iss_position)
+
+# -------- sunset and sunrise times API -------
+
+parameters = {
+    "lat" : MY_LAT,
+    "lng" : MY_LON,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
-
-longtitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longtitude, latitude)
-print(iss_position)
+print(data)
 
