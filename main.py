3d74b70341a616fc9555c2d3a73624da639f384f
RaihanKabir277 <raihank0198@gmail.com>
2025-09-03 09:50:54 +0600

    error handling batter way from request module documentation , fetch the json data, find the wanted values from them and print added here

--- main.py
+++ main.py
@@ -3,7 +3,36 @@
 import requests 
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# print(response)
 # print(type(response))
 
+# for get the response code 
+# print(response.status_code)
+
+# if response.status_code == 404:
+#     raise Exception("That resource does not exist")
+# elif response.status_code == 401:
+#     raise Exception("you are not authorised to access this data")
+
+#------------ there is a error handing in request documentation -------
+response.raise_for_status()
+
+# ----- json data fetch ----
+# data = response.json()
+# print(data)
+
+# ------ want to specific short of data -----
+# data = response.json()["iss_position"]
+# print(data)
+# data = response.json()["iss_position"]["longitude"]
+# print(data)
+
+# -------paricularly fetch longtitude and latitude -------
+data = response.json()
+
+longtitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longtitude, latitude)
+print(iss_position)
 
