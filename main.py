095760ab7bec6270014e6e3c6aa0a05b29a8e78f
RaihanKabir277 <raihank0198@gmail.com>
2025-09-03 09:15:07 +0600

    call api endpoint and take the response

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("day 33 with API starts here")
+# print("day 33 with API starts here")
+
+import requests 
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response)
+# print(type(response))
+
 
